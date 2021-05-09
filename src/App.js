d237e9289af15159f3376a56cd9f7c6d63b9421b
Gal Shimron <>
2021-05-09 12:16:22 +0300

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <HashRouter>
+        <Switch>
+          <Route exact path="LoginPage">Login</Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
