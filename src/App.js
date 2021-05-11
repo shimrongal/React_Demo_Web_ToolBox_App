389393a0a0908b17f261f2f2c4a4ecd794d90251
Gal Shimron <>
2021-05-11 13:16:16 +0300

    Get json data and create city name array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { HashRouter,  Route, Switch } from 'react-router-dom';
 import './App.css';
+import {  getCityList } from "./utils/HelperFunctions";
 
 import { AuthProvider } from "./utils/Auth";
 
@@ -27,6 +28,13 @@ function App() {
   
   const [userList , updateUsersList] = useState([[]]);
   const [shoppingList , updateShoppingList] = useState([new ShoppingItemModel('itemName','brand Name' , 1)]);
+
+  const [cityNameArr ,setCityNameArr] = useState();
+
+  //This code will get city list from json file for 
+  if (typeof cityNameArr === 'undefined'){
+    getCityList(setCityNameArr);
+  }
 
   return (
     <AuthProvider id="p-app-container">
