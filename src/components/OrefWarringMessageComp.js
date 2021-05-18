f3dcf9d9d6186a646eeb351a98dbfb26a6e04b40
Gal Shimron <>
2021-05-18 11:56:52 +0300

    Add Animation

--- src/components/OrefWarringMessageComp.js
+++ src/components/OrefWarringMessageComp.js
@@ -1,21 +1,11 @@
-
-let inlineStyleUl = {
-    width: "40%",
-    margin: "0 30%",
-    display: "flex",
-    justifyContent: "center"
-};
-
-let inlineStyleLi = {
-    flex: "1",
-    listStyle: "none"
-};
+import "../components/OrefWarrnigMessageComp.css"
 
 function OrefWarringMessageComp({item}) {
-    return <ul style={inlineStyleUl}>
-        <li style={inlineStyleLi}>{item.data}</li>
-        <li style={inlineStyleLi}>{item.alertDate}</li>
-        <li style={inlineStyleLi}>{item.title}</li>
+    
+    return <ul id="c-oref-warning-container" >
+        <li >{item.data}</li>
+        <li >{item.alertDate}</li>
+        <li >{item.title}</li>
 
     </ul> 
 }
