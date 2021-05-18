f252cb1f847becec433743003a1bfffdb03eae11
Gal Shimron <>
2021-05-18 19:45:30 +0300

    Remove Link to HomePage

--- src/components/NavBarComp.js
+++ src/components/NavBarComp.js
@@ -30,7 +30,6 @@ function NavBarComp() {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link as={Link}  to="/" >Home</Nav.Link>
         <Nav.Link as={Link}  to="/shopping_lists">Shopping list</Nav.Link>
         <Nav.Link as={Link}  to="/parking">Parking</Nav.Link>
       </Nav>
