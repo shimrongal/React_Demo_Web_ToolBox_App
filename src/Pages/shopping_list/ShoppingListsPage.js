87dcb675de0b0408e02fd34cd40d46b6f6561c2f
Gal Shimron <>
2021-05-18 17:30:35 +0300

    Add redirect on logout

--- src/Pages/shopping_list/ShoppingListsPage.js
+++ src/Pages/shopping_list/ShoppingListsPage.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getShoppingLists } from "../../utils/HelperFunctions";
 import 'font-awesome/css/font-awesome.min.css';
 import "./ShoppingListsPage.css";
 import NavBarComp from "../../components/NavBarComp";
 import ShoppingListItem from "../../components/shopping_item_comp/ShoppingListItem";
+import { AuthContext } from "../../utils/Auth";
+import { Redirect } from "react-router";
 
 function ShoppingListsPage() {
     
@@ -19,6 +21,11 @@ function ShoppingListsPage() {
     getShoppingLists(updateShoppingLists);
     },[]);
 
+    const {currentUser} = useContext(AuthContext);
+
+    if (typeof currentUser ==='undefined' || currentUser ===null) {
+      return <Redirect to="/login" />;
+    }
 
     return(<div className="p-container">
         <NavBarComp />
