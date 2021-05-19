bf3e7640e7e9d4b85cd0984afb800c78f5277474
Gal Shimron <>
2021-05-19 11:41:00 +0300

    remove Login alert after logout

--- src/Pages/home/HomePage.js
+++ src/Pages/home/HomePage.js
@@ -1,11 +1,11 @@
 import './HomePage.css';
-import  app   from "../../utils/FirebaseConfig";
 import { AuthContext } from "../../utils/Auth";
 import { Redirect } from 'react-router';
-import { useContext } from 'react';
-import { Button } from 'react-bootstrap';
-import NavBarComp from '../../components/NavBarComp';
+import { useContext, useEffect, useState } from 'react';
 import WeatherComp from '../../components/WeatherComp';
+import OrefWarringMessagesComp from '../../components/OrefWarringMessagesComp';
+import { orefWarningMessagesManager } from '../../utils/OrefWarningMessagesManager';
+
 
 /**
  * Created by Gal Shimron on 9/05/2021.
@@ -14,17 +14,26 @@ import WeatherComp from '../../components/WeatherComp';
  * @param {*} param0 
  */
 
-const HomePage = ({history , weatherObject}) => {
-  const {currentUser} = useContext(AuthContext);
+const HomePage = ({weatherObject, orefAlerts}) => {
+  const [currentOrefAlerts , updateOrefAlerts] = useState();
+  useEffect(()=>{
+    orefWarningMessagesManager(updateOrefAlerts) ;
+    const orefAlertsInterval = setInterval(()=>{
+      orefWarningMessagesManager(updateOrefAlerts) } , 5000) ;
+      return ()=>{
+          clearInterval(orefAlertsInterval);
+      }
 
-  if (typeof currentUser ==='undefined') {
-    return <Redirect to="/shopping_list" />;
+  },[]);
+
+  const {currentUser} = useContext(AuthContext);
+  if (typeof currentUser ==='undefined' || currentUser === null) {
+    return <Redirect to="/login" />;
   }
   return (
     <div >
-      <NavBarComp />
-      {typeof weatherObject !== 'undefined' ? <WeatherComp weatherObject={weatherObject} /> : <div>No Weather Data yet </div> }
-      <Button onClick={() => app.auth().signOut()}>Sign out</Button>
+      {typeof weatherObject !== 'undefined' ? <WeatherComp weatherObject={weatherObject}   /> : <div>No Weather Data yet </div> }
+      {typeof currentOrefAlerts    !== 'undefined' ? <OrefWarringMessagesComp alerts={currentOrefAlerts} /> : <div>No Alerts</div> }
     </div>
   );
 };
